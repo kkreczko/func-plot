de228065ccf6e65f1f6b27073e7491e2536b4dfb
Konrad Kreczko <konradkreczko@gmail.com>
2026-01-27 22:50:05 +0100

    fuck polish notation

--- eval.c
+++ eval.c
@@ -4,31 +4,7 @@
 #include <stdlib.h>
 
 double evalExpr(Node *expression, double value) { 
-  convertToLukasiewiczNotation(expression);
   return 10.0 * value; 
-}
-
-void convertToLukasiewiczNotation(Node *list) {
-  Node operatorStack[STACK_SIZE];
-  Node valueStack[STACK_SIZE];
-  size_t opStackPtr = 0;
-  size_t valStackPtr = 0;
-
-  while (list) {
-    if (isOperator(list)) {
-      // operatorStack[opStackPtr] = malloc(sizeof(Node));
-      operatorStack[opStackPtr] = *list;
-      opStackPtr++;
-    }
-
-    if (list->token == TOK_Number) {
-      // valueStack[valStackPtr] = malloc(sizeof(Node));
-      valueStack[valStackPtr] = *list;
-      valStackPtr++;
-    }
-
-    list = list->next;
-  }
 }
 
 int isOperator(Node *item) {
--- eval.h
+++ eval.h
@@ -5,9 +5,6 @@
 
 #define STACK_SIZE 169
 
-// polish notation correct name
-void convertToLukasiewiczNotation(Node *list);
-
 double evalExpr(Node *expression, double value);
 double evalSubExpr(Node *list);
 int isOperator(Node *item);
